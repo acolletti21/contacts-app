31a565f54ed971c7f537ee622b50340cb9d482b5
acolletti21 <ashleymcolletti@gmail.com>
2017-01-23 21:20:15 -0600

    added updated at, country code, and full name method"

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,43 @@
+class ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+  end
+
+  def create
+    @contact = Contact.new(
+                          first_name: params[:first_name],
+                          last_name: params[:last_name],
+                          email: params[:email],
+                          phone_number: params[:phone_number]
+                          )
+    @contact.save
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contacts.email = params[:email]
+    @contacts.phone_number = params[:phone_number]
+
+    @contact.save
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy 
+  end
+
+end
